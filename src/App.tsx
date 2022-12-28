f66a4a956f6a0ed9e3b7cf8459c23c911b22e548
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2022-12-28 18:55:02 -0500

    Initial commit - rick and morty app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Rick and Morty App
+    </div>
+  )
+}
+
+export default App
