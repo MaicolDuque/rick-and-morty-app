0fb359cf74c44f22dca522a646be9f176d28644c
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2022-12-30 09:08:41 -0500

    styled the list of characters

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,26 @@ import { useEffect } from "react"
 import styled from "styled-components";
 import { fetchCharacters } from "../../redux/actions/characters.actions";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import './styles.css'
 
 const CharactersContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+  padding-right: 20px;
 `
 
+const CharacterItem = styled.div`
+  min-height: 280px;
+  display: flex;
+  flex-direction: column;
+`
+
+const statusClass: Record<string, string> = {
+  'Alive': 'status-alive',
+  'Dead': 'status-dead',
+  'unknown': 'status-unknown'
+}
 export function Home(){
   const dispatch = useAppDispatch();
   const characters = useAppSelector(state => state.charactersReducer.characters)
@@ -21,7 +35,15 @@ export function Home(){
       <CharactersContainer>
       {
         characters.map((character: Record<string, string>) => {
-          return <div style={{ height: '100px'}} key={character.id}> {character.name}</div>
+          return (
+          <CharacterItem key={character.id}>
+            <img className="characterImage"  src={character.image} alt={character.name} />
+            <div className="character-item__info">
+              <div className="character-item__info-name">{character.name}</div>
+              <div><strong>Species:</strong> {character.species}</div>
+              <div><strong>Status:</strong> <span className={`character-item__info-status ${statusClass[character.status]}`}>{character.status}</span></div>
+            </div>
+          </CharacterItem>)
         })
       }
       </CharactersContainer>
